ce7d17334fd19e39a1e281c5dea601b7f9fd5a37
Mingy <wuzhengmao@dscomm.com.cn>
2012-10-16 17:54:44 +0800

    滚动时不再分栏

--- src/main/java/org/mingy/jmud/client/MudClient.java
+++ src/main/java/org/mingy/jmud/client/MudClient.java
@@ -9,6 +9,7 @@ import org.apache.commons.logging.LogFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.custom.StyledTextContent;
 import org.eclipse.swt.events.ControlAdapter;
 import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.events.KeyAdapter;
@@ -18,15 +19,12 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.FontData;
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.Sash;
 import org.eclipse.swt.widgets.ScrollBar;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
@@ -46,6 +44,10 @@ public class MudClient implements TelnetClientListener {
 	private static final String DEFAULT_FONT_FAMILY = "YaHei Consolas Hybrid";
 	/** 默认的字体大小 */
 	private static final int DEFAULT_FONT_SIZE = 10;
+	/** 默认的语种 */
+	private static final String DEFAULT_LOCALE = "zh_CN";
+	/** 默认的字符集 */
+	private static final String DEFAULT_CHARSET = "GBK";
 	/** 命令行历史记录数 */
 	private static final int COMMAND_HISTORY_SIZE = 20;
 	/** TAB的长度 */
@@ -59,18 +61,14 @@ public class MudClient implements TelnetClientListener {
 	private int connectTimeout;
 	/** 字符集 */
 	private Charset charset;
-	/** 主显示区域 */
-	private StyledText mainStyledText;
-	/** 滚动时的辅助显示区域 */
-	private StyledText scrolledStyledText;
-	/** 分隔栏 */
-	private Sash sash;
-	/** 纵向滚动条 */
-	private ScrollBar scrollBar;
+	/** 富文本显示区 */
+	private StyledText styledText;
 	/** 命令行输入框 */
 	private Text commandInput;
 	/** UI */
 	private Display display;
+	/** 富文本内容 */
+	private StyledTextContent content;
 	/** 默认的SGR */
 	private SGR defaultSGR;
 	/** 当前的SGR */
@@ -81,8 +79,10 @@ public class MudClient implements TelnetClientListener {
 	private LinkedList<String> commands;
 	/** 命令行历史选择时的指针 */
 	private int cmdptr;
-	/** 滚动时开启辅助显示区域，此时该值为true */
-	private boolean scrollLocked;
+	/** 富文本显示区是否可以滚动 */
+	private boolean canScroll;
+	/** 富文本显示区是否正在滚动 */
+	private boolean scrolling;
 	/** true时禁止显示接收到的数据 */
 	private boolean echoForbidden;
 
@@ -98,26 +98,17 @@ public class MudClient implements TelnetClientListener {
 	 * @param charset
 	 *            字符集
 	 * @param mainStyledText
-	 *            主显示区域
-	 * @param scrolledStyledText
-	 *            滚动时的辅助显示区域
-	 * @param sash
-	 *            分隔栏
-	 * @param scrollBar
-	 *            纵向滚动条
+	 *            富文本显示区
+	 * @param commandInput
+	 *            命令行输入框
 	 */
 	public MudClient(String hostname, int port, int connectTimeout,
-			String charset, StyledText mainStyledText,
-			StyledText scrolledStyledText, Sash sash, ScrollBar scrollBar,
-			Text commandInput) {
+			String charset, StyledText styledText, Text commandInput) {
 		this.hostname = hostname;
 		this.port = port;
 		this.connectTimeout = connectTimeout;
 		this.charset = Charset.forName(charset);
-		this.mainStyledText = mainStyledText;
-		this.scrolledStyledText = scrolledStyledText;
-		this.sash = sash;
-		this.scrollBar = scrollBar;
+		this.styledText = styledText;
 		this.commandInput = commandInput;
 		init();
 	}
@@ -178,34 +169,37 @@ public class MudClient implements TelnetClientListener {
 	}
 
 	private void init() {
-		display = mainStyledText.getDisplay();
-		mainStyledText.setEditable(false);
-		mainStyledText.setCaret(null);
-		mainStyledText.setTabs(TABS);
-		scrolledStyledText.setEditable(false);
-		scrolledStyledText.setCaret(null);
-		scrolledStyledText.setTabs(TABS);
-		scrollBar.setEnabled(false);
+		display = styledText.getDisplay();
+		content = styledText.getContent();
+		styledText.setEditable(false);
+		styledText.setCaret(null);
+		styledText.setTabs(TABS);
+		styledText.addControlListener(new ControlAdapter() {
+			@Override
+			public void controlResized(ControlEvent event) {
+				ScrollBar sb = styledText.getVerticalBar();
+				if (sb.getMinimum() + sb.getThumb() < sb.getMaximum()) {
+					canScroll = true;
+					if (!scrolling)
+						scrollToEnd();
+				} else {
+					canScroll = false;
+					scrolling = false;
+				}
+			}
+		});
+		styledText.getVerticalBar().addSelectionListener(
+				new SelectionAdapter() {
+					@Override
+					public void widgetSelected(SelectionEvent event) {
+						ScrollBar sb = styledText.getVerticalBar();
+						scrolling = sb.getSelection() + sb.getThumb() < sb
+								.getMaximum();
+					}
+				});
 		final ScrollListener scrollListener = new ScrollListener();
-		mainStyledText.getParent().addControlListener(new ControlAdapter() {
-			@Override
-			public void controlResized(ControlEvent e) {
-				if (scrollLocked)
-					unlockScroll();
-				scrollToEnd(mainStyledText);
-			}
-		});
-		mainStyledText.addListener(SWT.KeyDown, scrollListener);
-		mainStyledText.addListener(SWT.MouseWheel, scrollListener);
-		mainStyledText.setKeyBinding(SWT.PAGE_UP, SWT.NULL);
-		scrolledStyledText.addListener(SWT.KeyDown, scrollListener);
-		scrolledStyledText.addListener(SWT.MouseWheel, scrollListener);
-		scrollBar.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				// TODO Auto-generated method stub
-			}
-		});
+		styledText.addListener(SWT.KeyDown, scrollListener);
+		styledText.addListener(SWT.MouseWheel, scrollListener);
 		commandInput.addListener(SWT.KeyDown, scrollListener);
 		commandInput.addListener(SWT.MouseWheel, scrollListener);
 		commandInput.addKeyListener(new KeyAdapter() {
@@ -282,57 +276,8 @@ public class MudClient implements TelnetClientListener {
 		}
 	}
 
-	private void syncScroll(StyledText styledText) {
-		int n = styledText.getLineCount();
-		int y = styledText.getTopIndex();
-		int h = styledText.getLineIndex(styledText.getClientArea().height) - y;
-		if (n > h) {
-			scrollBar.setValues(y, 0, n - 1, h, 1, h);
-			scrollBar.setEnabled(true);
-		} else {
-			scrollBar.setEnabled(false);
-		}
-	}
-
-	private void lockScroll() {
-		scrollLocked = true;
-		if (!scrolledStyledText.isVisible()) {
-			if (logger.isTraceEnabled()) {
-				logger.trace("lock scroll");
-			}
-			scrolledStyledText.setText(mainStyledText.getText());
-			scrolledStyledText.setStyleRanges(mainStyledText.getStyleRanges());
-			scrolledStyledText.setVisible(true);
-			sash.setVisible(true);
-			FormData layoutData = (FormData) mainStyledText.getLayoutData();
-			layoutData.top.control = sash;
-			mainStyledText.setLayoutData(layoutData);
-			mainStyledText.getParent().layout();
-			scrollToEnd(mainStyledText);
-			scrollToEnd(scrolledStyledText);
-			syncScroll(scrolledStyledText);
-		}
-	}
-
-	private void unlockScroll() {
-		scrollLocked = false;
-		if (scrolledStyledText.isVisible()) {
-			if (logger.isTraceEnabled()) {
-				logger.trace("unlock scroll");
-			}
-			scrolledStyledText.setVisible(false);
-			sash.setVisible(false);
-			FormData layoutData = (FormData) mainStyledText.getLayoutData();
-			layoutData.top.control = null;
-			mainStyledText.setLayoutData(layoutData);
-			mainStyledText.getParent().layout();
-			scrollToEnd(mainStyledText);
-			syncScroll(mainStyledText);
-		}
-	}
-
 	private void scrollUp(int units) {
-		if (!scrollBar.isEnabled())
+		if (!canScroll)
 			return;
 		if (logger.isTraceEnabled()) {
 			if (units < 0)
@@ -340,23 +285,20 @@ public class MudClient implements TelnetClientListener {
 			else
 				logger.trace("scroll up: " + units);
 		}
-		if (!scrollLocked)
-			lockScroll();
-		int y = scrolledStyledText.getTopIndex();
-		int h = scrolledStyledText.getLineIndex(scrolledStyledText
-				.getClientArea().height) - y;
+		scrolling = true;
+		int y = styledText.getTopIndex();
+		int h = styledText.getLineIndex(styledText.getClientArea().height) - y;
 		y -= units < 0 ? h : units;
 		if (y < 0)
 			y = 0;
 		if (logger.isTraceEnabled()) {
 			logger.trace("scroll to line: " + y);
 		}
-		scrolledStyledText.setTopIndex(y);
-		scrollBar.setSelection(y);
+		styledText.setTopIndex(y);
 	}
 
 	private void scrollDown(int units) {
-		if (!scrollBar.isEnabled())
+		if (!scrolling)
 			return;
 		if (logger.isTraceEnabled()) {
 			if (units < 0)
@@ -364,37 +306,39 @@ public class MudClient implements TelnetClientListener {
 			else
 				logger.trace("scroll down: " + units);
 		}
-		if (scrollLocked) {
-			int n = scrolledStyledText.getLineCount();
-			int y = scrolledStyledText.getTopIndex();
-			int h = scrolledStyledText.getLineIndex(scrolledStyledText
-					.getClientArea().height) - y;
+		int n = styledText.getLineCount();
+		int y = styledText.getTopIndex();
+		int h = styledText.getLineIndex(styledText.getClientArea().height) - y;
+		if (y + h < n) {
 			y += units < 0 ? h : units;
-			if (y + h < n) {
-				if (logger.isTraceEnabled()) {
-					logger.trace("scroll to line: " + y);
-				}
-				scrolledStyledText.setTopIndex(y);
-				scrollBar.setSelection(y);
-			} else {
-				unlockScroll();
+			if (y + h >= n) {
+				y = n - h;
+				scrolling = false;
 			}
+			if (logger.isTraceEnabled()) {
+				logger.trace("scroll to line: " + y);
+			}
+			styledText.setTopIndex(y);
+		} else {
+			scrolling = false;
 		}
 	}
 
-	private void scrollToEnd(StyledText styledText) {
+	private void scrollToEnd() {
 		int n = styledText.getLineCount();
-		if (n > 0)
-			styledText.setTopIndex(n - 1);
+		int y = styledText.getTopIndex();
+		int h = styledText.getLineIndex(styledText.getClientArea().height) - y;
+		y = n - h;
+		if (logger.isTraceEnabled()) {
+			logger.trace("scroll to line: " + y);
+		}
+		styledText.setTopIndex(y);
 	}
 
 	private void initSGR() {
 		currentSGR = defaultSGR = new SGR(SGR.DEFAULT);
 		int[] rgb = defaultSGR.getBackgroundColor();
-		mainStyledText
-				.setBackground(new Color(display, rgb[0], rgb[1], rgb[2]));
-		scrolledStyledText.setBackground(new Color(display, rgb[0], rgb[1],
-				rgb[2]));
+		styledText.setBackground(new Color(display, rgb[0], rgb[1], rgb[2]));
 	}
 
 	private void processLine(byte[] bytes) throws Exception {
@@ -436,7 +380,7 @@ public class MudClient implements TelnetClientListener {
 				: currentSGR;
 		int[] textColor = sgr.getTextColor();
 		int[] bgColor = sgr.getBackgroundColor();
-		final StyleRange style = new StyleRange(mainStyledText.getCharCount(),
+		final StyleRange style = new StyleRange(styledText.getCharCount(),
 				message.length(), new Color(display, textColor[0],
 						textColor[1], textColor[2]), new Color(display,
 						bgColor[0], bgColor[1], bgColor[2]));
@@ -450,14 +394,18 @@ public class MudClient implements TelnetClientListener {
 			logger.trace(style);
 			logger.trace(message);
 		}
-		mainStyledText.append(message);
-		mainStyledText.setStyleRange(style);
-		scrollToEnd(mainStyledText);
-		if (scrollLocked) {
-			scrolledStyledText.append(message);
-			scrolledStyledText.setStyleRange(style);
+		styledText.append(message);
+		styledText.setStyleRange(style);
+		if (logger.isTraceEnabled()) {
+			logger.trace("lines: " + content.getLineCount());
 		}
-		syncScroll(scrollLocked ? scrolledStyledText : mainStyledText);
+		if (!canScroll) {
+			ScrollBar sb = styledText.getVerticalBar();
+			canScroll = sb.getMinimum() + sb.getThumb() < sb.getMaximum();
+		} else {
+			if (!scrolling)
+				scrollToEnd();
+		}
 	}
 
 	private static int matchEscStart(byte[] bytes, int start) {
@@ -487,56 +435,20 @@ public class MudClient implements TelnetClientListener {
 		Display display = new Display();
 		Shell shell = new Shell(display);
 		shell.setLayout(new FormLayout());
-		final Composite composite = new Composite(shell, SWT.V_SCROLL);
-		StyledText mainStyledText = new StyledText(composite, SWT.BORDER);
-		StyledText scrolledStyledText = new StyledText(composite, SWT.BORDER);
+		StyledText styledText = new StyledText(shell, SWT.BORDER | SWT.V_SCROLL);
 		FontData fontData = new FontData(DEFAULT_FONT_FAMILY,
 				DEFAULT_FONT_SIZE, SWT.NORMAL);
-		fontData.setLocale("zh_CN");
+		fontData.setLocale(DEFAULT_LOCALE);
 		Font font = new Font(display, fontData);
-		mainStyledText.setFont(font);
-		scrolledStyledText.setFont(font);
-		final Sash sash = new Sash(composite, SWT.HORIZONTAL);
-		composite.setLayout(new FormLayout());
-		FormData text1Data = new FormData();
-		text1Data.top = new FormAttachment(0, 0);
-		text1Data.bottom = new FormAttachment(sash, 0);
-		text1Data.left = new FormAttachment(0, 0);
-		text1Data.right = new FormAttachment(100, 0);
-		scrolledStyledText.setLayoutData(text1Data);
-		scrolledStyledText.setVisible(false);
-		final int limit = mainStyledText.getLineHeight(), percent = 66;
-		final FormData sashData = new FormData();
-		sashData.top = new FormAttachment(percent, 0);
-		sashData.left = new FormAttachment(0, 0);
-		sashData.right = new FormAttachment(100, 0);
-		sash.setLayoutData(sashData);
-		sash.setVisible(false);
-		sash.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event e) {
-				Rectangle sashRect = sash.getBounds();
-				Rectangle shellRect = composite.getClientArea();
-				int bottom = shellRect.height - sashRect.height - limit;
-				e.y = Math.max(Math.min(e.y, bottom), limit);
-				if (e.y != sashRect.y) {
-					sashData.top = new FormAttachment(0, e.y);
-					composite.layout();
-				}
-			}
-		});
-		FormData text2Data = new FormData();
-		text2Data.top = new FormAttachment(0, 0);
-		text2Data.bottom = new FormAttachment(100, 0);
-		text2Data.left = new FormAttachment(0, 0);
-		text2Data.right = new FormAttachment(100, 0);
-		mainStyledText.setLayoutData(text2Data);
+		styledText.setFont(font);
 		Text commandInput = new Text(shell, SWT.BORDER | SWT.SINGLE);
-		FormData compositeData = new FormData();
-		compositeData.top = new FormAttachment(0, 0);
-		compositeData.bottom = new FormAttachment(commandInput, 0);
-		compositeData.left = new FormAttachment(0, 0);
-		compositeData.right = new FormAttachment(100, 0);
-		composite.setLayoutData(compositeData);
+		commandInput.forceFocus();
+		FormData textData = new FormData();
+		textData.top = new FormAttachment(0, 0);
+		textData.bottom = new FormAttachment(commandInput, 0);
+		textData.left = new FormAttachment(0, 0);
+		textData.right = new FormAttachment(100, 0);
+		styledText.setLayoutData(textData);
 		FormData inputData = new FormData();
 		inputData.bottom = new FormAttachment(100, 0);
 		inputData.left = new FormAttachment(0, 0);
@@ -544,10 +456,8 @@ public class MudClient implements TelnetClientListener {
 		commandInput.setLayoutData(inputData);
 		shell.setSize(800, 600);
 		shell.open();
-		commandInput.forceFocus();
-		final MudClient mc = new MudClient("pkuxkx.net", 5555, 10000, "GBK",
-				mainStyledText, scrolledStyledText, sash,
-				composite.getVerticalBar(), commandInput);
+		final MudClient mc = new MudClient("pkuxkx.net", 5555, 10000,
+				DEFAULT_CHARSET, styledText, commandInput);
 		mc.connect();
 		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch())
